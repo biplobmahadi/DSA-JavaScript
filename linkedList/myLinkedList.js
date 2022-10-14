0d2412511f465293f423de240a8f5aae20d6448c
Mahadi Hasan Biplob <biplobhasan547@gmail.com>
2022-10-14 15:51:47 +0000

    update insert method with refactor in custom linked list

--- linkedList/myLinkedList.js
+++ linkedList/myLinkedList.js
@@ -36,18 +36,49 @@ class MyLinkedList {
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
-  }
+  } // O(1)
 
   prepend(value) {
     const newNode = new Node(value)
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
-  }
+  } // O(1)
+
+  printList() {
+    const list = []
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      list.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+    return list;
+  } // O(n)
+
+  insert(index, value) {
+    if (index <= 0) {
+      this.prepend(value)
+    } else if (index >= this.length) {
+      this.append(value)
+    } else {
+      let counter = 1;
+      let indexedNode = this.head;
+      while (counter !== index) {
+        counter++;
+        indexedNode = indexedNode.next;
+      }
+      const newNode = new Node(value);
+      newNode.next = indexedNode.next;
+      indexedNode.next = newNode;
+      this.length++;
+    }
+  } // O(n)
 }
 
 const myLinkedList = new MyLinkedList(10)
 myLinkedList.append(5)
 myLinkedList.append(15)
 myLinkedList.prepend(20)
-console.log(myLinkedList.head.next)
+myLinkedList.insert(2, 30)
+myLinkedList.insert(4, 50)
+console.log(myLinkedList.printList())
