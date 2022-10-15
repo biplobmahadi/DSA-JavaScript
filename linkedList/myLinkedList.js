501572213c426f62571d9a3b4cf1ace9725d01d4
Biplob <biplob.ruet15@gmail.com>
2022-10-15 11:07:06 +0600

    reverse a singly linked list

--- linkedList/myLinkedList.js
+++ linkedList/myLinkedList.js
@@ -17,7 +17,7 @@
 class Node {
   constructor(value) {
     this.value = value;
-    this.next = null
+    this.next = null;
   }
 }
 
@@ -25,32 +25,32 @@ class MyLinkedList {
   constructor(value) {
     this.head = {
       value: value,
-      next: null
-    }
+      next: null,
+    };
     this.tail = this.head;
-    this.length = 1
+    this.length = 1;
   }
 
   append(value) {
-    const newNode = new Node(value)
+    const newNode = new Node(value);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
   } // O(1)
 
   prepend(value) {
-    const newNode = new Node(value)
+    const newNode = new Node(value);
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
   } // O(1)
 
   printList() {
-    const list = []
+    const list = [];
     let currentNode = this.head;
     while (currentNode !== null) {
-      list.push(currentNode.value)
-      currentNode = currentNode.next
+      list.push(currentNode.value);
+      currentNode = currentNode.next;
     }
     return list;
   } // O(n)
@@ -67,11 +67,11 @@ class MyLinkedList {
 
   insert(index, value) {
     if (index <= 0) {
-      this.prepend(value)
+      this.prepend(value);
     } else if (index >= this.length) {
-      this.append(value)
+      this.append(value);
     } else {
-      const prevIndexedNode = this.findPrevIndexedNode(index)
+      const prevIndexedNode = this.findPrevIndexedNode(index);
 
       const newNode = new Node(value);
       newNode.next = prevIndexedNode.next;
@@ -89,21 +89,37 @@ class MyLinkedList {
       }
       this.length--;
     } else if (this.length >= index) {
-      const prevIndexedNode = this.findPrevIndexedNode(index)
+      const prevIndexedNode = this.findPrevIndexedNode(index);
 
       const unwantedNode = prevIndexedNode.next;
       prevIndexedNode.next = unwantedNode.next;
       this.length--;
     }
   } // O(n)
+
+  reverse() {
+    let prevNode = null;
+    let currentNode = this.head;
+    this.tail = currentNode;
+
+    while (currentNode !== null) {
+      let temp = currentNode.next;
+      currentNode.next = prevNode;
+      prevNode = currentNode;
+      currentNode = temp;
+    }
+    this.head = prevNode;
+    this.tail.next = null;
+  } // O(n) & O(1)
 }
 
-const myLinkedList = new MyLinkedList(10)
-myLinkedList.append(5)
-myLinkedList.append(15)
-myLinkedList.prepend(20)
-myLinkedList.insert(2, 30)
-myLinkedList.insert(4, 50)
-myLinkedList.remove(5)
-myLinkedList.remove(3)
-console.log(myLinkedList.printList(), myLinkedList.length)
+const myLinkedList = new MyLinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(15);
+myLinkedList.prepend(20);
+// myLinkedList.insert(2, 30);
+// myLinkedList.insert(4, 50);
+// myLinkedList.remove(5);
+// myLinkedList.remove(3);
+myLinkedList.reverse();
+console.log(myLinkedList.tail);
