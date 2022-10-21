018e63650dbb489aff4f26d88638d0f12aa48f77
Biplob <biplob.ruet15@gmail.com>
2022-10-21 12:23:17 +0600

    the best problem I ever solved, remove node from a binary search tree

--- tree/binarySearchTree.js
+++ tree/binarySearchTree.js
@@ -52,6 +52,115 @@ class BinarySearchTree {
     }
     return false;
   }
+
+  remove(value) {
+    // there is no root
+    if (!this.root) return false;
+    // there is only one node on root
+    if (!this.root.left && !this.root.right) {
+      this.root = null;
+      return this.root;
+    }
+    // there are child of root node
+    let currentNode = this.root;
+    let parentNode = null;
+    while (currentNode) {
+      if (currentNode.value > value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (currentNode.value < value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else {
+        // got match
+        // if there have no leaf of match
+        if (!currentNode.left && !currentNode.right) {
+          // check is it left of parent node or not
+          if (parentNode.left.value === currentNode.value) {
+            parentNode.left = null;
+            return this.root;
+          } else {
+            parentNode.right = null;
+            return this.root;
+          }
+        }
+        // if there are only one leaf(left or right)
+        if (currentNode.left && !currentNode.right) {
+          const replacedNode = currentNode.left;
+          // check current node is left of parent node or not
+          if (parentNode.left.value === currentNode.value) {
+            parentNode.left = replacedNode;
+            return this.root;
+          } else {
+            parentNode.right = replacedNode;
+            return this.root;
+          }
+        } else if (!currentNode.left && currentNode.right) {
+          const replacedNode = currentNode.right;
+          // check current node is left of parent node or not
+          if (parentNode.left.value === currentNode.value) {
+            parentNode.left = replacedNode;
+            return this.root;
+          } else {
+            parentNode.right = replacedNode;
+            return this.root;
+          }
+        }
+
+        // if there are two leaf
+        if (currentNode.left && currentNode.right) {
+          const subRoot = currentNode.right;
+          // if subRoot have no left
+          if (!subRoot.left) {
+            subRoot.left = currentNode.left;
+            // check current node is left of parent node or not
+            if (parentNode.left.value === currentNode.value) {
+              parentNode.left = subRoot;
+              return this.root;
+            } else {
+              parentNode.right = subRoot;
+              return this.root;
+            }
+          } else {
+            // sub root have left
+            // find min left node of subRoot
+            let currentMinNode = subRoot;
+            let parentMinNode;
+            while (currentMinNode.left) {
+              parentMinNode = currentMinNode;
+              currentMinNode = currentMinNode.left;
+            }
+            // if min node have no right
+            if (!currentMinNode.right) {
+              currentMinNode.left = currentNode.left;
+              currentMinNode.right = currentNode.right;
+              parentMinNode.left = null;
+              if (parentNode.left.value === currentNode.value) {
+                parentNode.left = currentMinNode;
+                return this.root;
+              } else {
+                parentNode.right = currentMinNode;
+                return this.root;
+              }
+            } else {
+              // if min node have right
+              parentMinNode.left = currentMinNode.right;
+              currentMinNode.left = currentNode.left;
+              currentMinNode.right = currentNode.right;
+              if (parentNode.left.value === currentNode.value) {
+                parentNode.left = currentMinNode;
+                return this.root;
+              } else {
+                parentNode.right = currentMinNode;
+                return this.root;
+              }
+            }
+          }
+        }
+      }
+    }
+    return false;
+  }
 }
 
 const bst = new BinarySearchTree();
@@ -60,12 +169,19 @@ bst.insert(4);
 bst.insert(6);
 bst.insert(20);
 bst.insert(170);
+bst.insert(180);
+bst.insert(160);
+bst.insert(165);
+bst.insert(155);
+bst.insert(157);
+bst.insert(156);
+bst.insert(158);
 bst.insert(15);
 bst.insert(1);
 
-console.log(bst.lookup(1));
+// console.log(bst.remove(20));
 
-// console.log(JSON.stringify(traverse(bst.root)));
+console.log(JSON.stringify(traverse(bst.remove(20))));
 
 function traverse(node) {
   const tree = { value: node.value };
